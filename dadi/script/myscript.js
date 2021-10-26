012ed34fa2b4ac695f78fce48b2ad570e5668514
Andrea Parisi <andreaparisi4502@gmail.com>
2021-10-26 16:18:26 +0200

    fix

--- dadi/script/myscript.js
+++ dadi/script/myscript.js
@@ -3,14 +3,14 @@ let numeroPc, numeroUtente;
 numeroPc = Math.floor(Math.random() * 6 + 1);
 numeroUtente = Math.floor(Math.random() * 6 + 1);
 
-console.log(numeroPc);
-console.log(numeroUtente);
+console.log("Questo è il numero del pc ",numeroPc);
+console.log("Questo è il tuo nomero ",numeroUtente);
 
-if( numeroPc > numeroUtente){
+if( numeroPc < numeroUtente){
 
     console.log("hai vinto!")
 
-}else if(numeroPc < numeroUtente){
+}else if(numeroPc > numeroUtente){
 
     console.log("hai perso!")
 
